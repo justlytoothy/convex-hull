3d1cba4b486d3b73130296bd85fd2ea177fe4659
Jake Schilling <jake@schillingcentral.net>
2023-10-03 10:39:04 -0600

    First

---
+++ convex_hull.py
@@ -0,0 +1,201 @@
+from which_pyqt import PYQT_VER
+
+if PYQT_VER == 'PYQT5':
+    from PyQt5.QtCore import QLineF, QPointF, QObject
+elif PYQT_VER == 'PYQT4':
+    from PyQt4.QtCore import QLineF, QPointF, QObject
+elif PYQT_VER == 'PYQT6':
+    from PyQt6.QtCore import QLineF, QPointF, QObject
+else:
+    raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
+
+import time
+
+# Some global color constants that might be useful
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Global variable that controls the speed of the recursion automation, in seconds
+PAUSE = 0.25
+
+
+class Node:
+    def __init__(self, point, next_node=None, prev_node=None):
+        self.point = point
+        self.next_node = next_node
+        self.prev_node = prev_node
+
+    def set_next_node(self, next_node):
+        self.next_node = next_node
+
+    def get_next_node(self):
+        return self.next_node
+
+    def set_prev_node(self, prev_node):
+        self.prev_node = prev_node
+
+    def get_prev_node(self):
+        return self.prev_node
+
+    def get_point(self):
+        return self.point
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head_node = None
+        self.tail_node = None
+
+    def add_to_head(self, new_value):
+        new_head = Node(new_value)
+        current_head = self.head_node
+
+        if current_head is not None:
+            current_head.set_prev_node(new_head)
+            new_head.set_next_node(current_head)
+
+        self.head_node = new_head
+
+        if self.tail_node is None:
+            self.tail_node = new_head
+
+    def add_to_tail(self, new_value):
+        new_tail = Node(new_value)
+        current_tail = self.tail_node
+
+        if current_tail is not None:
+            current_tail.set_next_node(new_tail)
+            new_tail.set_prev_node(current_tail)
+
+        self.tail_node = new_tail
+
+        if self.head_node is None:
+            self.head_node = new_tail
+
+    def remove_head(self):
+        removed_head = self.head_node
+
+        if removed_head == None:
+            return None
+
+        self.head_node = removed_head.get_next_node()
+
+        if self.head_node != None:
+            self.head_node.set_prev_node(None)
+
+        if removed_head == self.tail_node:
+            self.remove_tail()
+
+        return removed_head.get_value()
+
+    def remove_tail(self):
+        removed_tail = self.tail_node
+
+        if removed_tail is None:
+            return None
+
+        self.tail_node = removed_tail.get_prev_node()
+
+        if self.tail_node is not None:
+            self.tail_node.set_next_node(None)
+
+        if removed_tail == self.head_node:
+            self.remove_head()
+
+        return removed_tail.get_value()
+
+    def remove_by_value(self, value_to_remove):
+        node_to_remove = None
+        current_node = self.head_node
+
+        while current_node is not None:
+            if current_node.get_value() == value_to_remove:
+                node_to_remove = current_node
+                break
+
+            current_node = current_node.get_next_node()
+
+        if node_to_remove is None:
+            return None
+
+        if node_to_remove == self.head_node:
+            self.remove_head()
+        elif node_to_remove == self.tail_node:
+            self.remove_tail()
+        else:
+            next_node = node_to_remove.get_next_node()
+            prev_node = node_to_remove.get_prev_node()
+            next_node.set_prev_node(prev_node)
+            prev_node.set_next_node(next_node)
+
+        return node_to_remove
+
+
+#
+# This is the class you have to complete.
+#
+class ConvexHullSolver(QObject):
+
+    # Class constructor
+    def __init__(self):
+        super().__init__()
+        self.pause = False
+
+    # Some helper methods that make calls to the GUI, allowing us to send updates
+    # to be displayed.
+
+    def showTangent(self, line, color):
+        self.view.addLines(line, color)
+        if self.pause:
+            time.sleep(PAUSE)
+
+    def eraseTangent(self, line):
+        self.view.clearLines(line)
+
+    def blinkTangent(self, line, color):
+        self.showTangent(line, color)
+        self.eraseTangent(line)
+
+    def showHull(self, polygon, color):
+        self.view.addLines(polygon, color)
+        if self.pause:
+            time.sleep(PAUSE)
+
+    def eraseHull(self, polygon):
+        self.view.clearLines(polygon)
+
+    def showText(self, text):
+        self.view.displayStatusText(text)
+
+    def convex_solver(self, points):
+        if len(points) == 1:
+            return points
+        left_half = self.convex_solver(points[0: len(points) / 2])
+        right_half = self.convex_solver(points[len(points) / 2:])
+
+    # This is the method that gets called by the GUI and actually executes
+    # the finding of the hull
+    def compute_hull(self, points, pause, view):
+        self.pause = pause
+        self.view = view
+        assert (type(points) == list and type(points[0]) == QPointF)
+
+        t1 = time.time()
+        # TODO: SORT THE POINTS BY INCREASING X-VALUE
+        points.sort(key=lambda point: point.x)
+        t2 = time.time()
+
+        t3 = time.time()
+        # this is a dummy polygon of the first 3 unsorted points
+
+        polygon = [QLineF(points[i], points[(i + 1) % 3]) for i in range(3)]
+        # TODO: REPLACE THE LINE ABOVE WITH A CALL TO YOUR DIVIDE-AND-CONQUER CONVEX HULL SOLVER
+        # split array in half
+        # build doubly linked list
+        t4 = time.time()
+
+        # when passing lines to the display, pass a list of QLineF objects.  Each QLineF
+        # object can be created with two QPointF objects corresponding to the endpoints
+        self.showHull(polygon, RED)
+        self.showText('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t4 - t3))
