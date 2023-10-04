7ed76e2c732b32eadff0cf99c2590e54fe79f57c
Jake Schilling <jake@schillingcentral.net>
2023-10-04 11:47:04 -0600

    Firstnn

--- convex_hull.py
+++ convex_hull.py
@@ -20,121 +20,16 @@ BLUE = (0, 0, 255)
 PAUSE = 0.25
 
 
-class Node:
-    def __init__(self, point, next_node=None, prev_node=None):
-        self.point = point
-        self.next = next_node
-        self.prev = prev_node
-
-    def set_next_node(self, next_node):
-        self.next = next_node
-
-    def get_next_node(self):
-        return self.next
-
-    def set_prev_node(self, prev_node):
-        self.prev = prev_node
-
-    def get_prev_node(self):
-        return self.prev
-
-    def get_point(self):
-        return self.point
-
-
-class DoublyLinkedList:
-    def __init__(self):
-        self.head_node = None
-        self.tail_node = None
-
-    def add_to_head(self, new_value):
-        new_head = Node(new_value)
-        current_head = self.head_node
-
-        if current_head is not None:
-            current_head.set_prev_node(new_head)
-            new_head.set_next_node(current_head)
-
-        self.head_node = new_head
-
-        if self.tail_node is None:
-            self.tail_node = new_head
-
-    def add_to_tail(self, new_value):
-        new_tail = Node(new_value)
-        current_tail = self.tail_node
-
-        if current_tail is not None:
-            current_tail.set_next_node(new_tail)
-            new_tail.set_prev_node(current_tail)
-
-        self.tail_node = new_tail
-
-        if self.head_node is None:
-            self.head_node = new_tail
-
-    def remove_head(self):
-        removed_head = self.head_node
-
-        if removed_head is None:
-            return None
-
-        self.head_node = removed_head.get_next_node()
-
-        if self.head_node is not None:
-            self.head_node.set_prev_node(None)
-
-        if removed_head == self.tail_node:
-            self.remove_tail()
-
-        return removed_head.get_value()
-
-    def remove_tail(self):
-        removed_tail = self.tail_node
-
-        if removed_tail is None:
-            return None
-
-        self.tail_node = removed_tail.get_prev_node()
-
-        if self.tail_node is not None:
-            self.tail_node.set_next_node(None)
-
-        if removed_tail == self.head_node:
-            self.remove_head()
-
-        return removed_tail.get_value()
-
-    def remove_by_value(self, value_to_remove):
-        node_to_remove = None
-        current_node = self.head_node
-
-        while current_node is not None:
-            if current_node.get_value() == value_to_remove:
-                node_to_remove = current_node
-                break
-
-            current_node = current_node.get_next_node()
-
-        if node_to_remove is None:
-            return None
-
-        if node_to_remove == self.head_node:
-            self.remove_head()
-        elif node_to_remove == self.tail_node:
-            self.remove_tail()
-        else:
-            next_node = node_to_remove.get_next_node()
-            prev_node = node_to_remove.get_prev_node()
-            next_node.set_prev_node(prev_node)
-            prev_node.set_next_node(next_node)
-
-        return node_to_remove
-
-
 #
 # This is the class you have to complete.
 #
+def orientation(p, q, r):
+    val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y)
+    if val == 0:
+        return 0
+    return 1 if val > 0 else 2
+
+
 class ConvexHullSolver(QObject):
 
     # Class constructor
@@ -168,23 +63,46 @@ class ConvexHullSolver(QObject):
     def showText(self, text):
         self.view.displayStatusText(text)
 
-    # Find slope of line between two points
-    def findSlope(self, point_one, point_two):
-        return (point_one.y - point_two.y) / (point_one.x - point_two.x)
+    class Point:
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+            self.next = None
+            self.prev = None
 
-    def divide_points(self, points):
-        print("here")
-        if len(points) == 1:
-            print(points)
+    def convex_hull(self, points):
+        if len(points) <= 3:
             return points
-        left_half = self.divide_points(points[0: len(points) // 2])
-        right_half = self.divide_points(points[len(points) // 2:])
-        print(left_half)
-        return self.merge_hulls(left_half, right_half)
+        points.sort(key=lambda p: (p.x, p.y))
 
-    def merge_hulls(self, left, right):
-        print(left, right)
-        return left
+        # Initialize the doubly linked list
+        head = points[0]
+        tail = points[-1]
+        head.next = tail
+        tail.prev = head
+
+        upper_hull = [head, points[1]]
+
+        for i in range(2, len(points)):
+            while len(upper_hull) > 1 and orientation(upper_hull[-2], upper_hull[-1], points[i]) != 2:
+                upper_hull.pop()
+            upper_hull[-1].next = points[i]
+            points[i].prev = upper_hull[-1]
+            upper_hull.append(points[i])
+
+        lower_hull = [tail, points[-2]]
+
+        for i in range(len(points) - 3, -1, -1):
+            while len(lower_hull) > 1 and orientation(lower_hull[-2], lower_hull[-1], points[i]) != 2:
+                lower_hull.pop()
+            lower_hull[-1].next = points[i]
+            points[i].prev = lower_hull[-1]
+            lower_hull.append(points[i])
+
+        upper_hull[-1].next = lower_hull[0]
+        lower_hull[0].prev = upper_hull[-1]
+
+        return upper_hull + lower_hull
 
     # This is the method that gets called by the GUI and actually executes
     # the finding of the hull
@@ -199,8 +117,12 @@ class ConvexHullSolver(QObject):
 
         t3 = time.time()
         # this is a dummy polygon of the first 3 unsorted points
-        # polygon = [QLineF(points[i], points[(i + 1) % 3]) for i in range(3)]
-        polygon = self.divide_points(points)
+        converted_points = []
+        for point in points:
+            converted_points.append(self.Point(point[0], point[1]))
+        hull = self.convex_hull(converted_points)
+        polygon = [QLineF(QPointF(hull[i].x, hull[i].y),
+                          QPointF(hull[(i + 1) % len(hull)].x, hull[(i + 1) % len(hull)].y)) for i in range(len(hull))]
         # TODO: REPLACE THE LINE ABOVE WITH A CALL TO YOUR DIVIDE-AND-CONQUER CONVEX HULL SOLVER
         # split array in half
         # build doubly linked list
